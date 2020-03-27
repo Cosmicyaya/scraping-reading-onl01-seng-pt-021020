e33182f8d94c7be30bcbfb01c3bad9d332c61d2a
Ryan Hollins <ballbaroi@gmail.com>
2020-03-27 15:24:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-260IBN")
+html = open("https://flatiornschool.com/")
+doc = Nokoriri::HTML(html)
+
+doc.css(".<ul class="ctas-1fPiGw"><li><button class="buttonBlue-3yHkWz">Start Free Course</button></li></ul>")
